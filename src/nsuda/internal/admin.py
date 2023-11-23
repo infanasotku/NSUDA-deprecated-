6183c78e699a33d2fb2613109bca3d5bcbb859d7
InfaNaSotku <infanasotku@gmail.com>
2023-11-23 14:43:07 +0700

    Added 'add user' form

--- src/nsuda/internal/admin.py
+++ src/nsuda/internal/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response, status, Request
 from sqlalchemy.orm import Session
 from ..db import crud, schemas
 from hashlib import sha256
@@ -6,18 +6,29 @@ from hashlib import sha256
 
 router = APIRouter(prefix="/admin")
 
+from ..routers.roots import templates
 
 
 @router.get("/")
-async def index(response: Response, password: str = None, db: Session = Depends(crud.get_db)):
-    db_user = crud.get_user_by_email(db, id=1)
-    print(sha256(password).name)
+async def index(response: Response, request: Request, 
+                password: str = None, db: Session = Depends(crud.get_db), 
+                email: str = None,
+                added_days: int = None):
+    if email: # Email setted up by form
+        return email
+    db_user = crud.get_user(db, user_id=1)
     if db_user:
         if not password:
             response.status_code = status.HTTP_404_NOT_FOUND
-        elif sha256(password).name == db_user.password_hash:
-            return "Welcome to admin page!"
+        elif db_user.password_hash == "":
+            db_user.password_hash = sha256(password.encode('utf-8 ')).hexdigest()
+            crud.update_user(db, user=db_user)
+            return "New password set!"
+        elif sha256(password.encode('utf-8 ')).hexdigest() == db_user.password_hash:
+            return templates.TemplateResponse("admin.html", {"request": request, "password": password})
         else:
             response.status_code = status.HTTP_404_NOT_FOUND
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
+    return "Page not found"
+
