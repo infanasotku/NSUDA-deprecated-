e7cfcda00f68dc9dc0f629b110cb00a0d3249cf6
InfaNaSotku <gerasimov.mr@petrogm.com>
2023-11-24 04:13:37 +0700

    Add first worked gpt ver

--- src/gpt_module/main.py
+++ src/gpt_module/main.py
@@ -6,3 +6,12 @@ from gpt_module.routers import messenger
 
 app.include_router(messenger.router)
 
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+srcPath = Path(__file__).parent.parent.absolute()
+
+app.mount(
+    "/static",
+    StaticFiles(directory=srcPath / "static"),
+    name="static"
+)
--- src/gpt_module/routers/messenger.py
+++ src/gpt_module/routers/messenger.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import PlainTextResponse
 
+from pathlib import Path
+srcPath = Path(__file__).parent.parent.parent.absolute()
+
+templates = Jinja2Templates(directory=srcPath / "templates")
 
 router = APIRouter()
 
+import g4f
+
 @router.get("/")
-async def greet():
-    return "Greetings from GPT model!"
+async def greet(request: Request, msg=None, response_class=PlainTextResponse):
+    if not msg:
+        return templates.TemplateResponse("gpt.html", { "request": request })
+    else:
+        res = await g4f.ChatCompletion.create_async(
+            model=g4f.models.gpt_4,
+            provider=g4f.Provider.ChatBase,
+            messages=[{"role": "user", "content": msg}],
+        )
+        lines = res.split('\n')
+        return templates.TemplateResponse("gpt_answer.html", { "request": request, "lines": lines })
