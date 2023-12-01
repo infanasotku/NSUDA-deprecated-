8318d48cda132ee3e604c41525bc2c9eae07c879
InfaNaSotku <gerasimov.mr@petrogm.com>
2023-12-02 01:12:32 +0700

    Corrected load env in windows

--- src/xray_handler/handler.py
+++ src/xray_handler/handler.py
@@ -4,6 +4,9 @@ import subprocess as sproc
 from pathlib import Path
 import psutil
 from datetime import datetime
+import os
+import win32com.shell.shell as shell
+
 
 class Handler:
     def __init__(self, email: str):
@@ -27,6 +30,7 @@ class Handler:
             target=Handler._execute,
             daemon=True)
         self._xray_executor.start()
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f')
 
     def kill(self):
         process = psutil.Process(self._xray_executor.pid)
@@ -34,6 +38,7 @@ class Handler:
             proc.kill()
         process.kill()
         self._xray_executor.join()
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f')
 
     def check_status(self) -> bool: 
         try:
@@ -52,3 +57,15 @@ class Handler:
         
         self._xray_config = data["config"]
         self._last_update = data["last_update"]
+
+    def load_env_setting(self):
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v PrivacyAdvanced /t REG_DWORD /d 1 /f')
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v MigrateProxy /t REG_DWORD /d 1 /f')
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v EnableNegotiate /t REG_DWORD /d 1 /f')
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisableCachingOfSSLPages /t REG_DWORD /d 0 /f')
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v CertificateRevocation /t REG_DWORD /d 1 /f')
+        os.system(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d localhost:2081 /f')
+        self._run_as_admin(r'netsh winhttp import proxy source=ie /f')
+
+    def _run_as_admin(self, command: str):
+        shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+ command)
