1d5383aaa215991d2b896860347b04bb6e4cbbc2
InfaNaSotku <gerasimov.mr@petrogm.com>
2023-12-07 00:32:56 +0700

    Modifed building(removed extra files)

--- src/build.py
+++ src/build.py
@@ -23,19 +23,25 @@ if platform.system() == "Windows":
     BUILDER_ARGS.append(
         '--add-data=xray_handler/windows/*.dat:' + XRAY_FOLDER,
     )
-    BUILDER_ARGS.append(
-        '--add-data=xray_handler/windows/xray.exe:' + XRAY_FOLDER,
-    )
 elif platform.system() == "Darwin":
     XRAY_FOLDER = "mac_os"
     icon_name = "Logo.png"
     arch_folder: str = platform.uname().machine
-    BUILDER_ARGS.append(
-        '--add-data=xray_handler/mac_os/' + arch_folder + '/*.dat:' + XRAY_FOLDER,
-    )
+
     BUILDER_ARGS.append(
         '--add-data=xray_handler/mac_os/' + arch_folder + '/xray:' + XRAY_FOLDER,
     )
+elif platform.system() =="Linux":
+    XRAY_FOLDER = "linux"
+    icon_name = "Logo.png"
+
+    BUILDER_ARGS.append(
+        '--add-data=xray_handler/linux/xray:' + XRAY_FOLDER,
+    )
+
+BUILDER_ARGS.append(
+    '--add-data=xray_handler/*.dat:' + XRAY_FOLDER,
+)
 BUILDER_ARGS.append(
     '--icon=nsuda_client/resource/' + icon_name
 )
