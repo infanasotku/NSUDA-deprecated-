17983294b76bd04b33e8658fdeafe2108465ac01
InfaNaSotku <gerasimov.mr@petrogm.com>
2023-12-26 02:21:57 +0700

    Added VPN off when starting

--- src/xray_handler/windows/win_handler.py
+++ src/xray_handler/windows/win_handler.py
@@ -21,6 +21,7 @@ def load_env_setting():
     subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisableCachingOfSSLPages /t REG_DWORD /d 0 /f', creationflags=CREATE_NO_WINDOW)
     subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v CertificateRevocation /t REG_DWORD /d 1 /f', creationflags=CREATE_NO_WINDOW)
     subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d localhost:2081 /f', creationflags=CREATE_NO_WINDOW)
+    subprocess.call(r'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f', creationflags=CREATE_NO_WINDOW)
 
     shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+ r'netsh winhttp import proxy source=ie /f')
 
