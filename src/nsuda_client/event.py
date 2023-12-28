16bc642c869b068dc7409ecb2f717747fc3761fb
InfaNaSotku <infanasotku@gmail.com>
2023-12-29 02:07:32 +0700

    Added loading screen/Corrected position of ui elements

--- src/nsuda_client/event.py
+++ src/nsuda_client/event.py
@@ -1,8 +1,33 @@
 from xray_handler.messenger import MessengerBuilder
 import dearpygui.dearpygui as dpg
+from threading import Thread
 from config import window_width, window_height
 
+class UIData:
+    '''Keeps the UI data.
+    '''
+    def __init__(self):
+        self.load_screen_data = []
+
+class UIDataBuilder:
+    '''Provides access to UIData singleton instance.
+    '''
+    _instance:UIData = None
+
+    @staticmethod
+    def get_instanse():
+        if not UIDataBuilder._instance:
+            UIDataBuilder._instance = UIData()
+        return UIDataBuilder._instance
+
 small_font = None
+load_end_event = False
+
+def show_main_menu(show: bool):
+    dpg.configure_item(item="connect_button", show=show)
+    dpg.configure_item(item="remember_checkbox", show=show)
+    dpg.configure_item(item="email_item", show=show)
+    dpg.configure_item(item="password_item", show=show)
 
 def connect_button_clicked():
     email: str = dpg.get_value(item="email_item")
@@ -13,27 +38,36 @@ def connect_button_clicked():
     if dpg.get_value(item="remember_checkbox"):
         with open("login", "w") as f:
             f.write(f"{email} {password}")
+
+    show_main_menu(show=False)
+    global load_end_event
+    load_end_event = False
+    loading = Thread(daemon=True, target=view_load_screen)
+    loading.start()
     messenger = MessengerBuilder.get_instanse()
     msg: str = messenger.load_config(email=email, password=password)
+
     if msg != "OK":
+        load_end_event = True
+        loading.join()
+        show_main_menu(show=True)
         notice_for_error(msg)
         return
-    dpg.configure_item(item="connect_button", show=False)
-    dpg.configure_item(item="remember_checkbox", show=False)
-    dpg.configure_item(item="email_item", show=False)
-    dpg.configure_item(item="password_item", show=False)
-    dpg.configure_item(item="disconnect_button", show=True)
-    dpg.configure_item(item="connect_notice", show=True)
+
     messenger.run_session()
     messenger.handle_xray()
-    
+    load_end_event = True
+    loading.join()
+    dpg.configure_item(item="disconnect_button", show=True)
+    dpg.configure_item(item="connect_notice", show=True)
+
 def notice_for_error(notice: str):
     with dpg.window(
         width=350, 
         height=100, 
         no_resize=True,
         label="Notice",
-        pos=(window_width // 2 - 180, window_height // 2 - 90),
+        pos=(window_width // 2 - 175, window_height // 2 - 90),
         no_move=True,
         no_collapse=True
         ) as notice_window:
@@ -43,14 +77,30 @@ def notice_for_error(notice: str):
         dpg.bind_item_font(notice_window, small_font)
 
 def disconnect_button_clicked():
-    dpg.configure_item(item="connect_button", show=True)
-    dpg.configure_item(item="email_item", show=True)
-    dpg.configure_item(item="password_item", show=True)
+    show_main_menu(show=True)
     dpg.configure_item(item="connect_notice", show=False)
     dpg.configure_item(item="disconnect_button", show=False)
-    dpg.configure_item(item="remember_checkbox", show=True)
     MessengerBuilder.get_instanse().kill()
-    
+
+import time
+
+def view_load_screen():
+    dpg.configure_item(item="load_screen", show=True)
+    dpg.configure_item(item="connecting_notice", show=True)
+    ui_data = UIDataBuilder.get_instanse()
+    i = 0
+    while True:
+        for gif_data in ui_data.load_screen_data:
+            if load_end_event:
+                dpg.configure_item(item="load_screen", show=False)
+                dpg.configure_item(item="connecting_notice", show=False)
+                return
+            i += 1
+            i %= 40
+            dpg.configure_item(item="connecting_notice", 
+                               default_value="Connecting" + (i // 10) * '.')
+            dpg.set_value("load_screen_texture", gif_data)
+            time.sleep(0.02)
 
 def close_clicked():
     if MessengerBuilder.get_state():
--- src/nsuda_client/main.py
+++ src/nsuda_client/main.py
@@ -1,10 +1,26 @@
 import dearpygui.dearpygui as dpg
 import nsuda_client.event as event
 from config import *
-from xray_handler.messenger import os_handler
 
+
+
+def configure_load_screen():
+    ui_data = event.UIDataBuilder.get_instanse()
+    gif_width = None
+    gif_height = None
+    for i in range(1, 35):
+        gif_width, gif_height, _, gif_data = dpg.load_image(resource_path + f"/gif/flying-{i}.png")
+        ui_data.load_screen_data.append(gif_data)
+
+    with dpg.texture_registry(show=False):
+        gif_data = ui_data.load_screen_data[0]
+        dpg.add_dynamic_texture(width=gif_width,
+                                height=gif_height,
+                                    default_value=gif_data,
+                                    tag="load_screen_texture")
 
 def configure_nsuda():
+    configure_load_screen()
     with dpg.font_registry():
         default_font = dpg.add_font(resource_path + "/Gill Sans.ttf", 30)
         event.small_font = dpg.add_font(resource_path + "/Gill Sans.ttf", 21)
@@ -20,39 +36,48 @@ def configure_nsuda():
                 tag="email_item", 
                 hint="Input email",
                 default_value=email or "",
-                pos=(window_width // 2 - 180, window_height // 2 - 90),
+                pos=(window_width // 2 - 175, window_height // 2 - 90),
                 width=input_field_width
             )
         dpg.add_input_text(tag="password_item",
                 hint="Input password",
                 default_value=password or "",
-                pos=(window_width // 2 - 180, window_height // 2 - 40),
+                pos=(window_width // 2 - 175, window_height // 2 - 40),
                 width=input_field_width,
                 password=True
             )
         dpg.add_text(default_value="Connection established!", 
                 tag="connect_notice", 
-                pos=(window_width // 2 - 145, window_height // 2 - 60),
+                pos=(window_width // 2 - 140, window_height // 2 - 60),
                 show=False
             )
         dpg.add_button(label="Connect", 
                 callback=event.connect_button_clicked,
-                pos=(window_width // 2 - 85, window_height // 2 + 80), 
+                pos=(window_width // 2 - 82, window_height // 2 + 80), 
                 width=150, height=50,
                 tag="connect_button"
             )
         dpg.add_button(label="Disconnect", 
                 callback=event.disconnect_button_clicked,
-                pos=(window_width // 2 - 100, window_height // 2 + 40), 
+                pos=(window_width // 2 - 92, window_height // 2 + 40), 
                 width=180, height=50,
                 tag="disconnect_button",
                 show=False
             )
         dpg.add_checkbox(label="Remember me",
-                pos=(window_width // 2 - 120, window_height // 2 + 20),
+                pos=(window_width // 2 - 114, window_height // 2 + 20),
                 tag="remember_checkbox"
             )
         dpg.set_exit_callback(callback=event.close_clicked)
+        dpg.add_image(texture_tag="load_screen_texture",
+                      tag="load_screen",
+                      show=False,
+                      pos=(window_width // 2 - 101, window_height // 2 - 120))
+        dpg.add_text(default_value="Connecting", 
+                tag="connecting_notice", 
+                pos=(window_width // 2 - 70, window_height // 2 + 85),
+                show=False
+            )
 
     dpg.set_primary_window("Primary Window", True)
 from xray_handler.messenger import MessengerBuilder
