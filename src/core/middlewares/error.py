924a6192c332c8dcbeff48c0281e809e6053d9c8
InfaNaSotku <infanasotku@gmail.com>
2024-02-06 15:55:38 +0700

    Added error row handling

--- src/core/middlewares/error.py
+++ src/core/middlewares/error.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.responses import Response
+from starlette.responses import Response, HTMLResponse
 
 class HttpErrorMiddleware(BaseHTTPMiddleware):
     
@@ -9,15 +9,24 @@ class HttpErrorMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         response = await call_next(request)
-
+        
         if response.status_code < 400:
             return response
         elif response.status_code == 404:
             return self.error_404()
+        elif response.status_code == 500:
+            return self.error_500()
     
         
 
     def error_404(self) -> Response:
-        return Response(content="404", status_code=404)
+        return HTMLResponse(content="""
+    <h1>404</h1>
+""", status_code=404)
+    
+    def error_500(self) -> Response:
+        return HTMLResponse(content="""
+    <h1>500</h1>
+""", status_code=500)
 
 
