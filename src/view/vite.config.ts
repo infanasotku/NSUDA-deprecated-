8dd2e160a13c3442925ec0c9cc7d9757f4340a68
InfaNaSotku <infanasotku@gmail.com>
2024-02-08 16:37:34 +0700

    Deleted extra

--- src/view/vite.config.ts
+++ src/view/vite.config.ts
@@ -1,16 +1,54 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig, UserConfigFnObject } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+const config = defineConfig(({ command, mode }) => {
+  let proxyTarget: string
+  if (command === 'serve')
+  {
+    // Debug config
+    proxyTarget = 'http://localhost:5000'
+  }
+  else
+  {
+    proxyTarget = 'https://infanasotku.com'
+  }
+
+  return {
+    plugins: [
+          vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': proxyTarget
+      }
     }
   }
 })
+
+export default config
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   
+//   server: {
+//     port: 5001,
+//     proxy: (() =>
+//     {
+//       return {
+
+//       }
+//     })()
+//     // {
+//     //   '/api': {
+//     //     target: 'http://localhost:5000'
+//     //   }
+//     // }
+//   }
+// })
