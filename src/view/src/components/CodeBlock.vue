8b15864d31bdf0bdcc0cbba5308ea876829ddafb
InfaNaSotku <infanasotku@gmail.com>
2024-02-11 20:55:42 +0700

    Added code highlighting

---
+++ src/view/src/components/CodeBlock.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="code-block">
+        <span class="lang-label">{{ language }}</span>
+        <highlightjs class="code" :code="interiorCode"/>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'code-block',
+    data() {
+        return {
+            interiorCode: '',
+        }
+    },
+    props: {
+        language: {
+            type: String,
+            required: true
+        },
+        code: {
+            type: String,
+            required: true 
+        },
+        stream: {
+            type: Boolean
+        },
+        time: {
+            type: Number
+        },
+        onCodePrinted: {
+            type: Function
+        }
+    },
+    methods: {
+        startText(text: string, sleepTime: number) {
+            if (text.length === 0) {
+                if (this.onCodePrinted)
+                {
+                    this.onCodePrinted()
+                }
+                return
+            }
+            this.interiorCode += text[0]
+            setTimeout(() => { this.startText(text.substring(1, text.length),
+                 sleepTime) }, sleepTime)
+        }
+    },
+    mounted() {
+        if (this.stream)
+        {
+            this.startText(this.code, this.time! / this.code.length )
+        }
+        else
+        {
+            this.interiorCode = this.code
+        }
+    },
+})
+</script>
+<style>
+    .code-block
+    {
+        position: relative;
+    }
+    .code
+    {
+        --tw-shadow: 7px 7px 15px 0 #000;
+        box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);
+        margin: 0;
+        width: 100%;
+    }
+    .lang-label
+    {
+        font-family: 'ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace';
+        text-transform: uppercase;
+        position: absolute;
+        right: 0;
+        color: rgba(255, 255, 255, 0.8);
+        padding-top: 5px;
+        padding-bottom: 3px;
+        padding-right: 10px;
+        padding-left: 10px;
+        font-weight: 600;
+        font-size: 12px;
+        background-color: rgba(0,0,0,.3);
+        border-bottom-left-radius: 5px;
+    }
+</style>
--- src/view/src/main.ts
+++ src/view/src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router/router'
 import components from '@/components/UI'
+import 'highlight.js/styles/atom-one-dark.css'
+import 'highlight.js/lib/common';
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 const app = createApp(App)
 
@@ -9,4 +12,5 @@ components.forEach(component => app.component(component.name!, component));
 
 app.
 use(router).
+use(hljsVuePlugin).
 mount('#app')
