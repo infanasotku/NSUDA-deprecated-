9389c022acbb644d802d397a2ba204d79da902fc
InfaNaSotku <infanasotku@gmail.com>
2024-02-13 00:55:51 +0700

    Added pinia

--- src/view/src/main.ts
+++ src/view/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router/router'
 import components from '@/components/UI'
@@ -6,11 +7,13 @@ import 'highlight.js/styles/atom-one-dark.css'
 import 'highlight.js/lib/common';
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
+const pinia = createPinia()
 const app = createApp(App)
 
 components.forEach(component => app.component(component.name!, component));
 
 app.
+use(pinia).
 use(router).
 use(hljsVuePlugin).
 mount('#app')
