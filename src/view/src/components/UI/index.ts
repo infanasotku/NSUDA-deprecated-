8fa62d97c8330feb9c2dcf72feda40dc5af5856a
InfaNaSotku <infanasotku@gmail.com>
2024-02-15 16:51:59 +0700

    Started work with error line

--- src/view/src/components/UI/index.ts
+++ src/view/src/components/UI/index.ts
@@ -2,10 +2,12 @@ import DarkButton from "@/components/UI/DarkButton.vue"
 import LoadingIcon from "@/components/UI/LoadingIcon.vue"
 import TransparentButton from "@/components/UI/TransparentButton.vue"
 import TransparentInput from "./TransparentInput.vue"
+import ErrorLine from "./ErrorLine.vue"
 
 export default [
     DarkButton,
     LoadingIcon,
     TransparentButton,
-    TransparentInput
+    TransparentInput,
+    ErrorLine
 ]
