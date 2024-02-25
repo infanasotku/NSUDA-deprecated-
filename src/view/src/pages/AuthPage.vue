fe560eba4441b11c87eff53e7670a4a509531808
InfaNaSotku <infanasotku@gmail.com>
2024-02-25 21:46:41 +0700

    Started work with auth page

---
+++ src/view/src/pages/AuthPage.vue
@@ -0,0 +1,41 @@
+<template></template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+// Components
+import { AuthType } from '@/types';
+import { useAuthStore } from '@/store/auth';
+
+export default defineComponent({
+    props: {
+        authCode: {
+            type: String,
+            required: true
+        },
+        authService: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        console.log(this.authService)
+        console.log(this.authCode)
+        switch (this.authService) {
+            case 'google':
+                this.authStore.authenticateUser(AuthType.Google, this.authCode)
+                break;
+        
+            default:
+                break;
+        }
+    },
+    setup() {
+        const authStore = useAuthStore()
+
+        return {
+            authStore
+        }
+    }
+})
+</script>
+<style>
+</style>
--- src/view/src/router/router.ts
+++ src/view/src/router/router.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Main from '@/pages/MainPage.vue'
+import Auth from '@/pages/AuthPage.vue'
 
 const routes = [
     {
         path: '/',
-        component: Main,
+        component: Main
+    },
+    {
+        path: '/auth/',
+        component: Auth,
+        props: (route: any) => ({ authCode: route.query.code, authService: route.query.state })
     }
 ]
 
